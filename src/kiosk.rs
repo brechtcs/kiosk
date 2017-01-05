6959485c99fa7f193392c268d989709c9e83125e
Brecht Savelkoul <brecht.savelkoul@gmail.com>
2017-01-05 23:21:44 +0100

    Fix compilation error

--- src/kiosk.rs
+++ src/kiosk.rs
@@ -1,6 +1,7 @@
 use nickel::{Request, Response, MiddlewareResult};
+use reject::not_allowed;
 
-pub fn get<'app>(_req: &mut Request, res: Response<'app>) -> MiddlewareResult<'app> {
+pub fn get<'app>(req: &mut Request, res: Response<'app>) -> MiddlewareResult<'app> {
   if !req.origin.remote_addr.ip().is_loopback() {
     return not_allowed(res);
   }
